82e4b0fd0a65fed8f7f54720cad1e2c29230f404
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-18 18:59:59 +0600

    while loop concept clear

---
+++ Module-9.3.js
@@ -0,0 +1,36 @@
+// todo:   Module no - 9.3      topic: Problem-solving with While Loop. 
+
+// for loop and while loop same to same : 
+
+// for(let i = 1 ; i <= 10; i ++ ){
+//     console.log('inside for loop - ', i);
+// }
+
+
+//* while loop: 
+/**
+ * 1. initialization.
+ * 2. while(){}
+ * 3.condition 
+ * 4. console.log('inside while loop');
+ * 5.  modification .
+ */
+
+// increment: 
+let i = 1 ;
+
+while( i <= 10){
+    console.log('inside the while loop -', i);
+    i++;
+}
+
+
+// decrement while loop:
+let number = 10;
+
+while( number >=  1 ){
+    console.log('inside the while loop - ', number);
+    number-- ;
+}
+
+
