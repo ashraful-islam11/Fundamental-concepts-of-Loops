a5803be0ad6a84b10f2fd10b8db1f1307d61b40e
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-18 23:31:33 +0600

    Different ways to use loops

---
+++ Module-9.6.js
@@ -0,0 +1,32 @@
+// todo:       Module no - 9.6          topic: Different Ways to Use Loops.
+
+// for loop decrement : 
+
+for(let num= 20; num >= 1; num--){
+    console.log(num);
+}
+
+// for loop decrement : 20- 11
+console.log('  for loop decrement : 20- 11');
+
+for(let numb= 20; numb >= 11; numb--){
+    console.log(numb);
+}
+
+
+console.log( '4 er ghorer namota : ');
+
+let table4 = 4;
+
+for(number = 1; number<= 10; number ++){
+    console.log( table4 , 'X', number , '=', table4 *number);
+}
+
+
+console.log('10 er ghorer namota :');
+
+let table10 = 10;
+for(let i = 1; i<=10; i++){
+    console.log(table10 , 'X', i, '=', table10 *i );
+}
+
