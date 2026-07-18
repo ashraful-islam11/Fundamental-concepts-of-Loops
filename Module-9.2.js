806ddde2d11badcf09e14ef686cf4b817d90800f
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-18 18:32:11 +0600

    Introduction to while loop

---
+++ Module-9.2.js
@@ -0,0 +1,50 @@
+//  todo:      Module no - 9.2         topic:   Introduction to While Loop  .
+
+/**
+ * *    while loop : যতক্ষন condition true হবে ততক্ষন loop চলবেই :
+ * 
+ * while loop: structure :    
+ * 
+ * 
+ * 
+ * 1. Initialization : 
+ * 
+ * 2. while(){}
+ * 
+ * 3. condition: 
+ * 
+ * 4. modification :
+ * 
+ */
+
+
+// *  while loop structure : 
+// let number = 10; // initialization: 
+
+// while(condition){ // condition: 
+//     // while loop body :
+//     console.log('your need output '); // our need 
+
+//     increment/ decrement // modification:
+// }
+
+
+//* while loop is start : increment: 
+
+let count = 1;
+
+while (count <= 10){
+    console.log('1-10 print use while loop', count);
+
+    count++;
+}
+
+
+//* while loop is start : decrement: 
+
+let number = 20;
+while(number >= 10){
+    console.log('20-10 print use while loop',number);
+    number--;
+}
+
