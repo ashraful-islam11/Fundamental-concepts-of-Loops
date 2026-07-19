e1555d900d1eace2c96549b362ef62a765fbeee5
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 18:03:02 +0600

    module summery  all loops

---
+++ Module-9.9.js
@@ -0,0 +1,25 @@
+// todo:   Module no -9.9           topic: Difference between types of loops and Module Summary.
+
+// 1. for loop: 
+console.log('for loop: ');
+for(let i = 0; i<=10; i++){
+    console.log('the value of for loop : ', i);
+}
+
+
+// 2. while loop :
+console.log('while loop: '); 
+let n = 1;
+while(n <= 10){
+    console.log('the value of while loop :', n);
+    n++;
+}
+
+
+// 3. do while :
+console.log('do while loop: ');
+let number = 1;
+do{
+    console.log('the value of do while loop:', number);
+    number++;
+}while(number<=10);
